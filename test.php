6c386dbbc91108e71673b8f97ad0ede9ce134c8c
Kai Xin <s10255744@connect.np.edu.sg>
2025-06-10 11:40:27 +0800

    Fixed Some Errrors

--- test.php
+++ test.php
@@ -1,8 +1,7 @@
 <?php
-$conn = new mysqli("db", "user", "password", "Verztec");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+$mysqli = new mysqli('db', 'user', 'password', 'Verztec');
+if ($mysqli->connect_error) {
+    die('Connection failed: ' . $mysqli->connect_error);
+}
+echo '✅ Connected successfully!';
 ?>
